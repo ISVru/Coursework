7727d176e5b408174d9815d7192184b788921f58
ISVru <isv_ru@mail.ru>
2022-01-05 17:17:05 +0300

    +CW_VUE-CLI

---
+++ CW_VUE-CLI/blog/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <nav class="nav">
+    <router-link class="link nav__link" to="/catalog">Товары</router-link>
+    <router-link class="link nav__link" to="/cart">Корзина: {{ this.$store.getters.cartCount }}
+    </router-link>
+  </nav>
+  <router-view/>
+</template>
+<style>
+.nav {
+  text-align: center;
+  margin-bottom: 18px;
+}
+.link {
+  text-decoration: none;
+  color: #864b4b;
+}
+.link:hover {
+  color: #554f4f;
+}
+.nav__link {
+  font-size: 1.5rem;
+  padding: 0 10px;
+}
+</style>
